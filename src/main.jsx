87cf286cdc02281bbb2e7f93ebb726eacd359d53
Muhammadamin Anvarov <anvarovmuhammmadamin011@gmail.com>
2026-08-07 00:35:27 +0500

    Root element etishmayotganida turg'un mount qo'shildi

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,26 @@ import './app.css'
 import App from './App.jsx'
 import useStore from './store/useStore'
 
+function mount() {
+  const container = document.getElementById('root')
+  if (!container) {
+    requestAnimationFrame(mount)
+    return
+  }
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
 async function start() {
   try {
     await useStore.getState().boot()
   } catch {
     /* demo rejimida davom etamiz */
   }
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
+  mount()
 }
 
 start()
